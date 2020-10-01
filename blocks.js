a5453d62adaa113fa8d1a18d7ee66203777aaa58
GovindSarda48 <70316473+GovindSarda48@users.noreply.github.com>
2020-10-01 20:08:03 +0530

    Add files via upload

---
+++ blocks.js
@@ -0,0 +1,35 @@
+class Blocks{
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        var options = {
+          isStatic:false,
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("BLOCKS.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        pop();
+
+        stroke("black");
+        strokeWeight(4);
+
+        fill("pink");
+    
+      
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+       
+      }
+}
